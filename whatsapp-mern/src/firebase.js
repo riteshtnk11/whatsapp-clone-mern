b5785b1f42c6ca618035b6f2f04f497ae02cc1e9
Ritesh <riteshtnk11@gmail.com>
2020-10-10 22:12:03 +0700

    [ADD] Adding firebase config file in app

---
+++ whatsapp-mern/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDhHY5e2HD4esdo00M6ughAtqMd3vEkWZU",
+    authDomain: "whatsapp-clone-mern-29081.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-mern-29081.firebaseio.com",
+    projectId: "whatsapp-clone-mern-29081",
+    storageBucket: "whatsapp-clone-mern-29081.appspot.com",
+    messagingSenderId: "529103115792",
+    appId: "1:529103115792:web:b42f12cef2e1feed1e5c95",
+    measurementId: "G-7VK9189DS8"
+  };
